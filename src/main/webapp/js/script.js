7d8fe435609770287dcbb080c85ed94910544095
SELPHA <sellyn92@gmail.com>
2018-03-14 10:30:04 +0300

    first commit

---
+++ src/main/webapp/js/script.js
@@ -0,0 +1,29 @@
+/**
+ * Created by SELPHA on 13/3/2018.
+ */
+var ws;
+
+function connect() {
+    var username = document.getElementById("username").value;
+    ws = new WebSocket("ws://" + document.location.host + "/thegroup/chat/" + username);
+
+
+    ws.onmessage = function(event) {
+        var log = document.getElementById("log");
+        console.log(event.data);
+        var message = JSON.parse(event.data);
+        log.innerHTML += message.from + " : " + message.content + "\n";
+    };
+}
+
+function send() {
+    var content = document.getElementById("msg").value;
+    var to = document.getElementById("to").value;
+    var json = JSON.stringify({
+        "to":to,
+        "content":content
+    });
+
+    ws.send(json);
+    log.innerHTML += "Me : " + content + "\n";
+}
